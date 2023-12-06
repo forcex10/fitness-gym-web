270f4be2f9b916640b0f6415bfcfbf4d20744cb8
roxemir200 <127453187+roxemir200@users.noreply.github.com>
2023-12-06 17:36:21 +0100

    Add files via upload

---
+++ View/listJoueurs.php
@@ -0,0 +1,40 @@
+<html>
+<body>
+    <h1>List of players</h1>
+    <h2>
+        <a href="addJoueur.php">Add player</a>
+    </h2>
+
+<table border="1" align="center" width="70%">
+    <tr>
+        <th>Id Joueur</th>
+        <th>Nom</th>
+        <th>Prenom</th>
+        <th>Email</th>
+        <th>Tel</th>
+        <th>Update</th>
+        <th>Delete</th>
+    </tr>
+
+
+   
+
+
+
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                
+            </td>
+            <td>
+                
+            </td>
+        </tr>
+   
+</table></body>
+</html>
